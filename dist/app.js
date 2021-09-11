f5944effc172e9f8c75582086be57e5fe54801c5
benoit <benoit.gallou0gmail.com>
2021-09-11 12:21:46 +0200

    ajout config nodemon

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
 var port = 3000;
+app.get('/player', function (req, res) {
+    res.send('Les joueurs');
+});
+app.get('/team', function (req, res) {
+    res.send('Les équipes!!');
+});
 app.get('/', function (req, res) {
-    res.send('The sedulous hyena ate the antelope!');
+    res.send('The sedulous hyena ate the antelope.');
 });
 app.listen(port, function () {
     return console.log("server is listening on " + port);
--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,19 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+
+app.get('/player', (req, res) => {
+  res.send('Les joueurs');
 });
+
+app.get('/team', (req, res) => {
+  res.send('Les équipes!!');
+});
+
+app.get('/', (req, res) => {
+  res.send('The sedulous hyena ate the antelope.');
+});
+
 
 app.listen(port, () => {
 
